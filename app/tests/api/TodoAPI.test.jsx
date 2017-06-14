02f3c0692bef2e5ef278448e53ab1ca41c38ccf6
Prathap Chilukuri <pcha01@L-IDC0C8FD59-M.local>
2017-06-14 19:56:20 +0530

    add tests for TodoAPI

---
+++ app/tests/api/TodoAPI.test.jsx
@@ -0,0 +1,56 @@
+var expect = require('expect');
+
+var TodoAPI = require('TodoAPI');
+
+describe('TodoAPI', () => {
+  beforeEach(() => {
+      localStorage.removeItem('todos');
+  });
+
+  it('should exist', () => {
+    expect(TodoAPI).toExist();
+  });
+
+  describe('setTodos', () => {
+    it('should set valid todos array', () => {
+      var todos = [{
+        id: 23,
+        text: 'test',
+        completed: false
+      }];
+      TodoAPI.setTodos(todos);
+
+      var actualTodos = JSON.parse(localStorage.getItem('todos'));
+
+      expect(actualTodos).toEqual(todos);
+    });
+
+    it('should return undefined for invalid todos array', () => {
+      var todos = {};
+      TodoAPI.setTodos(todos);
+
+      expect(localStorage.getItem('todos')).toBe(null);
+    });
+  });
+
+  describe('getTodos', () => {
+    it('should return empty array for bad localstorage data', () => {
+      var actualTodos = TodoAPI.getTodos();
+
+      expect(actualTodos).toEqual([]);
+    });
+
+    it('should return todo if valid array in localstorage', () => {
+      var todos = [{
+        id: 23,
+        text: 'test',
+        completed: false
+      }];
+      localStorage.setItem('todos', JSON.stringify(todos));
+
+      var actualTodos = TodoAPI.getTodos();
+
+      expect(actualTodos).toEqual(todos);
+    });
+  });
+});
