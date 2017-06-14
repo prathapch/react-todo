b7ec58e524225d80147f910d2e12556b1201ff91
Prathap Chilukuri <pcha01@L-IDC0C8FD59-M.local>
2017-06-15 00:33:18 +0530

    complete filter functionality

--- app/tests/api/TodoAPI.test.jsx
+++ app/tests/api/TodoAPI.test.jsx
@@ -53,4 +53,51 @@ describe('TodoAPI', () => {
       expect(actualTodos).toEqual(todos);
     });
   });
+
+  describe('filterTodos', () => {
+    var todos = [{
+      id: 1,
+      text: 'text1',
+      completed: true
+    },{
+      id: 2,
+      text: 'text2',
+      completed: false
+    },{
+      id: 3,
+      text: 'text3',
+      completed: true
+    }];
+
+    it('should return all items if showCompleted is true', () => {
+      var filteredTodos = TodoAPI.filterTodos(todos, true, '');
+
+      expect(filteredTodos.length).toBe(3);
+    });
+
+    it('should return non-completed todos if showCompleted is false', () => {
+      var filteredTodos = TodoAPI.filterTodos(todos, false, '');
+
+      expect(filteredTodos.length).toBe(1);
+    });
+
+    it('should sort by completed status', () => {
+      var filteredTodos = TodoAPI.filterTodos(todos, true, '');
+
+      expect(filteredTodos.length).toBe(3);
+      expect(filteredTodos[0].completed).toBe(false);
+      expect(filteredTodos[1].completed).toBe(true);
+      expect(filteredTodos[2].completed).toBe(true);
+    });
+
+    it('should filter todos by searchText', () => {
+      var filteredTodos = TodoAPI.filterTodos(todos, true, 't1');
+      expect(filteredTodos.length).toBe(1);
+    });
+
+    it('should return all todos if searchText is empty', () => {
+      var filteredTodos = TodoAPI.filterTodos(todos, true, '');
+      expect(filteredTodos.length).toBe(3);
+    });
+  });
 });
